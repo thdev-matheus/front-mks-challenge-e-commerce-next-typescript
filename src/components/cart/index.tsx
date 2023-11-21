080c14f88c7cba48f4c8968dbde5e20c0e186339
Theus <matheusth.dev@gmail.com>
2023-11-21 01:47:42 -0300

    feat: cart header and footer

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,18 +1,72 @@
 import { useCart } from "@/contexts";
 import * as S from "./styles";
+import { IoMdCloseCircle } from "react-icons/io";
+import { useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 interface ICartProps {
   toggleIsCartOpen: () => void;
 }
 
 export const Cart = ({ toggleIsCartOpen }: ICartProps) => {
-  const { cart } = useCart();
+  const { cart, removeFromCart, resetCart, totalPay } = useCart();
+  const controll = useAnimation();
+
+  const variants = {
+    hidden: { x: 500, opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+  };
+
+  const transition = {
+    duration: 0.5,
+    ease: "easeInOut",
+    type: "spring",
+    damping: 12,
+    stiffness: 150,
+  };
+
+  useEffect(() => {
+    controll.start("visible");
+  }, []);
 
   return (
     <S.Container>
-      <S.deadArea onClick={toggleIsCartOpen} />
+      <S.deadArea
+        onClick={() => {
+          controll.start("hidden").then((_) => toggleIsCartOpen());
+        }}
+      />
 
-      <S.CartBox></S.CartBox>
+      <S.CartBox
+        variants={variants}
+        transition={transition}
+        initial="hidden"
+        animate={controll}
+      >
+        <S.CartHeader>
+          <h3>Carrinho de compras</h3>
+          <IoMdCloseCircle
+            onClick={() => {
+              controll.start("hidden").then((_) => toggleIsCartOpen());
+            }}
+          />
+        </S.CartHeader>
+
+        <S.CartMain></S.CartMain>
+
+        <S.CartFooter>
+          <button className="reset_cart" onClick={resetCart}>
+            Esvaziar Carrinho
+          </button>
+
+          <S.BoxTotal>
+            <h4>Total:</h4>
+            <h4>R$ {totalPay.toString().replace(".", ",")}</h4>
+          </S.BoxTotal>
+
+          <button className="purchase">Finalizar Compra</button>
+        </S.CartFooter>
+      </S.CartBox>
     </S.Container>
   );
 };
